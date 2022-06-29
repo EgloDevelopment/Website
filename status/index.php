a1a8e81143b27f559156dd4899b9d819eddaa244
Eglo <105808341+EgloDevelopment@users.noreply.github.com>
2022-06-29 00:23:06 -0600

    Status page update

--- status/index.php
+++ status/index.php
@@ -3,7 +3,7 @@
 <iframe style="width: 100%; height: 300px;" allowfullscreen src="//umap.openstreetmap.fr/en/map/untitled-map_781884?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=false&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
 </div>
 
-<iframe style="width: 100%; height: 400px; margin-bottom: 300px;" frameborder="0" allowfullscreen src="https://cron.eglo.pw/servers.html"></iframe>
+<iframe style="width: 100%; height: 600px; margin-bottom: 100px;" frameborder="0" allowfullscreen src="https://cron.eglo.pw/servers.html"></iframe>
 </div>
 
 <?php
