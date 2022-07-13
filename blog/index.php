fa314ad611693003b8659f7744a0500b900df0f4
Eglo <105808341+EgloDevelopment@users.noreply.github.com>
2022-07-13 17:24:29 -0600

    Added blog and blog post

---
+++ blog/index.php
@@ -0,0 +1,53 @@
+<?php include("../resources/headers/header.php"); ?>
+<body width="device-width">
+<div class="space">
+    <h1 id="txt3">Eglo Blog.</h1>
+    <h5 id="txt2">Welcome to the Eglo Blog, where we post tech updates, news about Eglo, and just memes <i>sometimes</i> ;)</h5>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h4 id="txt2">View our most recent story,</h4>
+
+
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Nymi</h5>
+        <p class="card-text">Our recent addition to the Eglo line of products. a free and self hostable cloud storage platform.<br><br></p>
+        <div class="btn-group dropup">
+          <button type="button" class="btn btn-primary" onclick="window.location='https://eglo.pw/blog/nymi-cloud-storage';">Read</button>
+        </div>
+      </div>
+    </div>
+
+    
+
+
+  </div>
+  <hr style="width: 100%; opacity: 0;">
+  <div class="space">
+    <h4 id="txt2">View our other posts,</h4>
+
+
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Nymi</h5>
+        <p class="card-text">Our recent addition to the Eglo line of products. a free and self hostable cloud storage platform. <br><br></p>
+        <div class="btn-group dropup">
+          <button type="button" class="btn btn-primary" onclick="window.location='https://eglo.pw/blog/nymi-cloud-storage';">Read</button>
+        </div>
+      </div>
+    </div>
+
+    
+
+
+  </div>
+</body>
+
+
+
+<div style="margin-top: 750px;">
+<?php include("../resources/headers/footer.php"); ?>
+</div>
