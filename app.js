1402527c5c7f14d650d3fe5885ffe4104403650b
EgloDevelopment <UNUstifA4W346TcF2Qwc@gmail.com>
2022-08-15 21:01:33 -0600

    Finished

--- app.js
+++ app.js
@@ -39,7 +39,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000, function(){
-    console.log("info",'Server is running at port : ' + 3000);
+
+port = 3000;
+
+app.listen(port, function(){
+    console.log("Info:",'Server is running at port: ' + port);
 });
 module.exports = app;
